9835298ea5acf508c94949294dfd7c6186d09e28
vishal karhad <vishalkarhad199@gmail.com>
2024-09-23 15:17:42 +0530

    add iris classification project among there css and html file

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.request import urlopen as uReq
 import os
+import numpy
+from joblib import dump,load
+from sklearn.datasets import load_iris
 
 app= Flask(__name__)
 
@@ -26,6 +29,44 @@ def search():
     for i in images_tages:
         images_url=i['src']
         return render_template("images_page.html",name1=images_tages ,image_name=query)
+    
+
+@app.route("/iris",methods=['GET'])
+def iris():
+    
+    return render_template("iris_form.html")
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    model=load("knn1.joblib")
+    if model is None:
+        return render_template('iris_form.html', prediction="Model is not available.")
+
+    try:
+        # Retrieve and convert input values
+        sepal_length = float(request.form['sepal_length'])
+        sepal_width = float(request.form['sepal_width'])
+        petal_length = float(request.form['petal_length'])
+        petal_width = float(request.form['petal_width'])
+
+        # Create input features
+        input_features = [[sepal_length, sepal_width, petal_length, petal_width]]
+        
+        # Make prediction
+        prediction = model.predict(input_features)
+        iris=load_iris()
+
+        flower_names = iris.target_names
+        flower_prediction = flower_names[prediction[0]]
+
+        return render_template('iris_form.html', prediction=flower_prediction)
+
+    except ValueError:
+        return render_template('iris_form.html', prediction="Invalid input values. Please enter numeric values.")
+    except Exception as e:
+        return render_template('iris_form.html', prediction=f"Error: {str(e)}")
+
 
 if __name__=="__main__":
     app.run(debug=True)
